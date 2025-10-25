33649803a7ca616a05242ad059a16a9657e60a4b
Kyllian Senrens <kylliansenrens3004@gmail.com>
2025-10-25 19:08:58 +0200

    refactor: improve SVG formatting and ensure consistent structure in contact section

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,20 +1,20 @@
 export const WORKER_AGENT_NAME = 'sendo-worker';
 
 export const SOCIAL_LINKS = {
-    'Twitter': {
-        url: 'https://x.com/SendoMarket',
+	Twitter: {
+		url: 'https://x.com/SendoMarket',
 		username: 'SendoMarket',
-    },
-    'Farcaster': {
-        url: 'https://tr.ee/2qngnmSoK1',
-        username: 'sendomarket',
-    },
-    'Discord': {
-        url: 'https://discord.gg/Anf3rkF2s',
-        username: 'discord.gg/Anf3rkF2s',
-    },
-    'Email': {
-        url: 'sendo.market@proton.me',
-        username: 'sendo.market@proton.me',
-    },
+	},
+	Farcaster: {
+		url: 'https://tr.ee/2qngnmSoK1',
+		username: 'sendomarket',
+	},
+	Discord: {
+		url: 'https://discord.gg/Anf3rkF2s',
+		username: 'discord.gg/Anf3rkF2s',
+	},
+	Email: {
+		url: 'sendo.market@proton.me',
+		username: 'sendo.market@proton.me',
+	},
 };
